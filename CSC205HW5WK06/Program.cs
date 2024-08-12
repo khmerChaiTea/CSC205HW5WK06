2add06f044fc8cfc12b3c4c1c52c79f4a9cdf8f7
pchai <prempreychhay@gmail.com>
2024-08-12 10:49:32 -0700

    Working with numbers

--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -10,13 +10,30 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            // Working with string
-            string phrase = "FreeCodeCamp Academy";
-            Console.WriteLine(phrase[13]);   // Put the index of the string, start with 0
-            Console.WriteLine(phrase.IndexOf("Academy"));   // Give the starting index of the word
-            Console.WriteLine(phrase.IndexOf('z')); // Output -1 because it is not existed
-            Console.WriteLine(phrase.Substring(13));
-            Console.WriteLine(phrase.Substring(13, 3));    // Gab only three characters
+            // Working with numbers
+            int num = 6;
+            Console.WriteLine(4 + 2 * 3);
+            Console.WriteLine((4 + 2) * 3);
+            Console.WriteLine(5.0 + 8.1);   // Get a decimal
+            Console.WriteLine(5 + 8.1);     // Get a decimal
+            Console.WriteLine(5 / 2);       // Get an integer
+            Console.WriteLine(num);
+            num++;
+            Console.WriteLine(num);
+            ++num;
+            Console.WriteLine(num);
+            num--;
+            Console.WriteLine(num);
+            --num;
+            Console.WriteLine(num);
+
+            // Methods
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3,2));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Min(4, 90));
+            Console.WriteLine(Math.Round(4.6));
 
             Console.ReadLine();
         }
