47db6611d7b01612bba2e99d7e27bd24a6cca2d9
pchai <prempreychhay@gmail.com>
2024-08-12 11:27:03 -0700

    Building a Calculator

--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -10,13 +10,18 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            // Getting User Input
-            Console.WriteLine("Hello"); // Next output go to a new line
-            Console.Write("Enter your name: ");    // Next output stay on the same line
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " you are " + age);
+            // Building a Calculator
+            int num = Convert.ToInt32("45");    // Convert string into an integer -- only work with number only
+            //int num = Convert.ToInt32("56sadf");  // Will not work because is not number only
+            Console.WriteLine("43" + "56"); // Concatenate - output ast string 4356
+            Console.WriteLine(num + 6);
+
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine()); // Change int into double and convert ToDouble for decimal input
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
 
             Console.ReadLine();
         }
