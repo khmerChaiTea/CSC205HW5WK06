1d95205a0c355b6bac5f981bf251531cedff2c73
pchai <prempreychhay@gmail.com>
2024-08-12 09:53:34 -0700

    More Console.WriteLine output.

--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -10,10 +10,10 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            Console.WriteLine("There once was a man named George");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name George");
+            Console.WriteLine("But didn't like being 70");
 
             Console.ReadLine();
         }
