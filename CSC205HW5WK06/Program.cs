cbe3880c5905265680a1d477039b2fd8b07ccf2a
pchai <prempreychhay@gmail.com>
2024-08-12 20:08:49 -0700

    If Statement

--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -10,16 +10,42 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            // Return Statement
-            Console.WriteLine(cube(5));
+            // If Statement
+            bool isMale = false;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
 
             Console.ReadLine();
         }
-
-        static int cube(int num)
-        {
-            int result = num * num * num;
-            return result;
-        }
     }
 }
+///sumary///
+/// I wake up
+/// If I'm hungry
+///     I eat breakfast
+///     
+/// I look at my phone
+/// if it's about to die
+///     I charge it
+///     
+/// I leave my house
+/// if it;s cloudy
+///     i bring an unbrella
+/// otherwise
+///     I bring sunglasses
