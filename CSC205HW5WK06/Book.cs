690966a8bb39a26b3cde3b7bed2c83aedb79b87d
pchai <prempreychhay@gmail.com>
2024-08-13 08:33:42 -0700

    Classes & Objects

---
+++ CSC205HW5WK06/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205HW5WK06
+{
+    public class Book
+    {
+        // Class attributes
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -11,31 +11,26 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            // Exception Handling
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+            // Classes & Objects
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;  // Instance of the book class
 
-                Console.WriteLine(num1 / num2);
-            }
-            catch (DivideByZeroException e) // Specific exception is the better to code
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            //finally // Always going to get executed
-            //{
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;  // Instance of the book class
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            Console.WriteLine();
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
 
             Console.ReadLine();
         }
