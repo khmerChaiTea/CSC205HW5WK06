2e2fb5dbc8d63109ed76a5b2c95304087d5293f6
pchai <prempreychhay@gmail.com>
2024-08-12 21:27:33 -0700

    2d Arrays

--- CSC205HW5WK06/Program.cs
+++ CSC205HW5WK06/Program.cs
@@ -11,22 +11,18 @@ namespace CSC205HW5WK06
     {
         static void Main(string[] args)
         {
-            // Building an Exponent Method
-            Console.WriteLine(GetPow(4, 3)); // Can only be used for positive number
+            // 2d Arrays
+            int[,] numberGrid = {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 }
+            };
+            int[,] myArray = new int[2, 3];
+
+            Console.WriteLine(numberGrid[1,1]);
+            Console.WriteLine(numberGrid[2, 1]);
 
             Console.ReadLine();
-        }
-
-        static int GetPow(int baseNum, int powNum)
-        {
-            int result = 1;
-
-            for (int i = 0; i < powNum; i++)
-            {
-                result *= baseNum;  // result = result * baseNum; 
-            }
-
-            return result;
         }
     }
 }
